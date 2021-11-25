89df14f77f69b491fefb9ccafd0a1163c3a4b539
OmarJ <omarjandali93@gmail.com>
2021-11-25 00:24:49 -0800

    added more templates

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def home_page():
 @app.route('/donations', methods=['GET'])
 def all_donations():
   all_donations = donations.find()
-  return render_template('all_donations.html', all_donations=all_donations)
+  all_charities = charities.find()
+  return render_template('all_donations.html', all_donations=all_donations, all_charities=all_charities)
 
 # Grabs all of the charities
 @app.route('/charities', methods=['GET'])
@@ -32,22 +33,26 @@ def all_charities():
 # Create single donation
 @app.route('/donations', methods=['POST'])
 def create_donation():
-  if request.form.get('charity_name') == '':
+  if request.form.get('charity') == '':
     return render_template('new_charity.html')
   else:
     new_donation = {
-      'charity_name': request.form.get('charity_name'),
+      'charity_id': request.form.get('charity'),
       'amount': request.form.get('amount'),
       'notes': request.form.get('notes'),
-      'user_name': request.form.get('user_name')
+      'user_name': request.form.get('name')
     }
-    single_charity = charities.find_one({'name': ObjectId(request.form.get('charity_name'))}) 
-    single_charity['total_donations'] = single_charity['total_donations'] + 1
-    single_charity['total_donated'] = single_charity['total_donated'] + request.form.get('amount')
+    single_charity = charities.find_one({'_id': ObjectId(request.form.get('charity'))})
+    charity_donations = int(single_charity['total_dontations'])
+    charity_donated = int(single_charity['total_donated'])
+    single_charity['total_dontations'] = charity_donations + 1
+    single_charity['total_donated'] = charity_donated + int(request.form.get('amount'))
     single_charity['all_donations'].append(new_donation)
-  all_donations = donations.find()
-  all_charities = charities.find()
-  return render_template('home_page.html', donations=all_donations, charities=all_charities)
+    charities.update_one({'_id': ObjectId(request.form.get('charity'))}, {'$set': single_charity})
+    donations.insert_one(new_donation)
+    all_donations = donations.find()
+    all_charities = charities.find()
+    return render_template('home_page.html', all_donations=all_donations, all_charities=all_charities)
 
 # Create single charity
 @app.route('/charities', methods=['POST'])
@@ -65,7 +70,7 @@ def create_charity():
   charities.insert_one(new_charity)
   all_donations = donations.find()
   all_charities = charities.find()
-  return render_template('home_page.html', donations=all_donations, charities=all_charities)
+  return render_template('home_page.html', all_donations=all_donations, all_charities=all_charities)
 
 
 #update a donatoin
@@ -100,13 +105,15 @@ def update_charity(charity_id):
   return render_template('charity_single.html', single_charity=single_charity)
 
 # Donation Deletion
-@app.route('/donations/<donation_id>', methods=['DELETE'])
+@app.route('/donations/delete/<donation_id>', methods=['GET'])
 def donation_delete(donation_id):
   donation_to_delete = donations.find_one({'_id': ObjectId(donation_id)})
-  charity_to_update = charities.find_one({'name': donation_to_delete['charity_name']})
-  charity_to_update['total_donations'] = charity_to_update['total_donations'] - 1
-  charity_to_update['total_donated'] = charity_to_update['total_donated'] - donation_to_delete['amount']
+  charity_to_update = charities.find_one({'_id': ObjectId(donation_to_delete['charity_id'])})
+  charity_to_update['total_dontations'] = int(charity_to_update['total_dontations']) - 1
+  charity_to_update['total_donated'] = int(charity_to_update['total_donated']) - int(donation_to_delete['amount'])
+  print(type(charity_to_update['all_donations']))
   charity_to_update['all_donations'].remove(donation_to_delete)
+  charities.update_one({'_id': ObjectId(request.form.get('charity'))}, {'$set': charity_to_update})
   donations.delete_one({'_id': ObjectId(donation_id)})
   all_donations = donations.find()
   return render_template('all_donations.html', all_donations=all_donations)
@@ -125,7 +132,7 @@ def charity_delete(charity_id):
   return render_template('all_charities.html', all_charities=all_charities)
 
 # Grabs single donation
-@app.route('/donations/delete/<donation_id>', methods=['GET'])
+@app.route('/donations/<donation_id>', methods=['GET'])
 def single_donation(donation_id):
   single_donation = donations.find_one({'_id': ObjectId(donation_id)})
   return render_template('donation_single.html', single_donation=single_donation)
